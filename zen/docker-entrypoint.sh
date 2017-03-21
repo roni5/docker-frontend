aef8c5184a610e8f8dada4a456cd291577e55df0
Pablo Guerino <pablo.guerino@jadelabs.net>
2017-03-21 12:45:36 -0300

    docker-frontend : clean up & updates

--- zen/docker-entrypoint.sh
+++ zen/docker-entrypoint.sh
@@ -3,31 +3,6 @@
 # prevent conflicts with existing
 rm -rf node_modules
 
-/usr/bin/npm link \
-  bower \
-  breakpoint-sass \
-  browser-sync \
-  chroma-sass \
-  del \
-  eslint \
-  event-stream \
-  gulp \
-  gulp-autoprefixer \
-  gulp-eslint \
-  gulp-if \
-  gulp-load-plugins \
-  gulp-rename \
-  gulp-replace \
-  gulp-sass \
-  gulp-sass-lint \
-  gulp-size \
-  gulp-sourcemaps \
-  gulp.spritesmith \
-  kss \
-  node-sass-import-once \
-  sass-lint \
-  support-for \
-  typey \
-  zen-grids
+/usr/bin/npm link $NPM_PACKAGES
 
 exec "$@"
